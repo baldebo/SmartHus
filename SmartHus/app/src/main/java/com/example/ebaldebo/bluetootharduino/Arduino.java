f6b689dd83c4a947b643ef7f1ae355a598ab75a8
Emil <ebaldebo@bitbucket.org>
2016-12-14 03:32:17 +0100

    Major fixes

--- SmartHus/app/src/main/java/com/example/ebaldebo/bluetootharduino/Arduino.java
+++ SmartHus/app/src/main/java/com/example/ebaldebo/bluetootharduino/Arduino.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.Bundle;
 import android.app.Activity;
 import android.speech.RecognizerIntent;
@@ -20,6 +21,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.UUID;
@@ -27,7 +30,7 @@ import java.util.UUID;
 public class Arduino extends Activity {
 
     //Alla variabler
-    TextView lampa1, lampa2, lampa3, temp, window, larm;
+    TextView lampa1, lampa2, lampa3, temp, window, larm, testa;
     private String address = null;
     private static ProgressDialog progress;
     private BluetoothSocket btSocket = null;
@@ -35,6 +38,11 @@ public class Arduino extends Activity {
     private static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private boolean disconnected = false;
     private final int REQ_CODE_SPEECH_INPUT = 20;
+    Handler bluetoothIn;
+    private ConnectedThread mConnectedThread;
+    private StringBuilder recDataString = new StringBuilder();
+
+    final int handlerState = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +63,49 @@ public class Arduino extends Activity {
         lampa1 = (TextView)findViewById(R.id.lampa1);
         lampa2 = (TextView)findViewById(R.id.lampa2);
         lampa3 = (TextView)findViewById(R.id.lampa3);
+        testa = (TextView)findViewById(R.id.header);
         temp = (TextView)findViewById(R.id.temp);
         window = (TextView)findViewById(R.id.window);
         larm = (TextView)findViewById(R.id.larm);
+
+        bluetoothIn = new Handler(new Handler.Callback() {
+            public boolean handleMessage(android.os.Message msg) {
+                if(msg.what == handlerState) {
+                    String readMessage = (String) msg.obj;
+                    recDataString.append(readMessage);
+                    int endOfLineIndex = recDataString.indexOf("Q");
+                    if (endOfLineIndex > 0) {
+                        if(recDataString.charAt(0) == 'P') {
+                            String lampa1Value = recDataString.substring(1,2);
+                            String lampa2Value = recDataString.substring(2,3);
+                            String lampa3Value = recDataString.substring(3,4);
+                            String tempValue = recDataString.substring(4,6);
+                            String windowValue = recDataString.substring(6,7);
+                            String larmValue = recDataString.substring(7,8);
+
+                            if (lampa1Value.equals("1")) lampa1.setText("Lampa 1 PÅ");
+                                else lampa1.setText("Lampa 1 AV");
+                            if (lampa2Value.equals("1")) lampa2.setText("Lampa 2 PÅ");
+                                else lampa2.setText("Lampa 2 AV");
+                            if (lampa3Value.equals("1")) lampa3.setText("Lampa 3 PÅ");
+                                else lampa3.setText("Lampa 3 AV");
+                            temp.setText(tempValue + (char)0x00B0 + "C");
+                            if (windowValue.equals("1")) window.setText("Fönster öppet!");
+                                else window.setText("Fönster stängt.");
+                            if (larmValue.equals("1")) larm.setText("Larm på.");
+                                else larm.setText("Larm av.");
+
+                        }
+                        recDataString.delete(0, recDataString.length());
+
+
+                    }
+
+
+                }
+                return true;
+            }
+        });
 
         //Använd bakgrundsklassen Bluetooth för att ansluta.
         new Bluetooth().execute();
@@ -145,87 +193,64 @@ public class Arduino extends Activity {
             case REQ_CODE_SPEECH_INPUT : {
                 if(resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    if(result.get(0).equals(commands[0])) {
-                        if(btSocket != null) {
-                            try {
-                                btSocket.getOutputStream().write("1".getBytes());
-                            } catch(IOException e) {
-                                Toast.makeText(getApplicationContext(), "FEL!", Toast.LENGTH_LONG).show();
-                            } break;
-                        }
+                    if(result.get(0).equals(commands[0]) || result.get(0).equals(commands[1])) {
+                        mConnectedThread.write("1");
                     }
 
-                    if(result.get(0).equals(commands[1])) {
-                        if(btSocket != null) {
-                            try {
-                                btSocket.getOutputStream().write("2".getBytes());
-                            } catch(IOException e) {
-                                Toast.makeText(getApplicationContext(), "FEL!", Toast.LENGTH_LONG).show();
-                            } break;
-                        }
+                    if(result.get(0).equals(commands[2]) || result.get(0).equals(commands[3])) {
+                        mConnectedThread.write("2");
                     }
 
-                    if(result.get(0).equals(commands[2])) {
-                        if(btSocket != null) {
-                            try {
-                                btSocket.getOutputStream().write("3".getBytes());
-                            } catch(IOException e) {
-                                Toast.makeText(getApplicationContext(), "FEL!", Toast.LENGTH_LONG).show();
-                            } break;
-                        }
+                    if(result.get(0).equals(commands[4]) || result.get(0).equals(commands[5])) {
+                        mConnectedThread.write("3");
                     }
 
-                    if(result.get(0).equals(commands[3])) {
-                        if(btSocket != null) {
-                            try {
-                                btSocket.getOutputStream().write("4".getBytes());
-                            } catch(IOException e) {
-                                Toast.makeText(getApplicationContext(), "FEL!", Toast.LENGTH_LONG).show();
-                            } break;
-                        }
-                    }
-
-                    if(result.get(0).equals(commands[4])) {
-                        if(btSocket != null) {
-                            try {
-                                btSocket.getOutputStream().write("5".getBytes());
-                            } catch(IOException e) {
-                                Toast.makeText(getApplicationContext(), "FEL!", Toast.LENGTH_LONG).show();
-                            } break;
-                        }
-                    }
-
-                    if(result.get(0).equals(commands[5])) {
-                        if(btSocket != null) {
-                            try {
-                                btSocket.getOutputStream().write("6".getBytes());
-                            } catch(IOException e) {
-                                Toast.makeText(getApplicationContext(), "FEL!", Toast.LENGTH_LONG).show();
-                            } break;
-                        }
-                    }
-
-                    if(result.get(0).equals(commands[6])) {
-                        if(btSocket != null) {
-                            try {
-                                btSocket.getOutputStream().write("7".getBytes());
-                            } catch(IOException e) {
-                                Toast.makeText(getApplicationContext(), "FEL!", Toast.LENGTH_LONG).show();
-                            } break;
-                        }
-                    }
-
-                    if(result.get(0).equals(commands[7])) {
-                        if(btSocket != null) {
-                            try {
-                                btSocket.getOutputStream().write("8".getBytes());
-                            } catch(IOException e) {
-                                Toast.makeText(getApplicationContext(), "FEL!", Toast.LENGTH_LONG).show();
-                            } break;
-                        }
+                    if(result.get(0).equals(commands[6]) || result.get(0).equals(commands[7])) {
+                        mConnectedThread.write("4");
                     }
                 }
 
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final InputStream inPut;
+        private final OutputStream outPut;
+
+        ConnectedThread(BluetoothSocket sockerino) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = sockerino.getInputStream();
+                tmpOut = sockerino.getOutputStream();
+            } catch(IOException e){
+                Toast.makeText(getApplicationContext(), "Något gick väldigt fel!", Toast.LENGTH_LONG).show();
+            }
+            inPut = tmpIn;
+            outPut = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = inPut.read(buffer);
+                    String readMessage = new String(buffer, 0 , bytes);
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {break;}
+            }
+        }
+        void write(String input) {
+            byte[] msgBuffer = input.getBytes();
+            try {
+                outPut.write(msgBuffer);
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "Detta fungerar inte...", Toast.LENGTH_LONG).show();
+                finish();
             }
         }
     }
@@ -251,6 +276,9 @@ public class Arduino extends Activity {
                     btSocket = hc06.createInsecureRfcommSocketToServiceRecord(myUUID);
                     BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
                     btSocket.connect();
+
+                    mConnectedThread = new ConnectedThread(btSocket);
+                    mConnectedThread.start();
                 }
             } catch(IOException e) {
                 ansluten = false;
--- btAndroid/btAndroid.ino/btAndroid.ino.ino
+++ btAndroid/btAndroid.ino/btAndroid.ino.ino
@@ -1,157 +1,163 @@
-/* Av Emil Baldebo 04/12/2016
-Komponenter:
-4 LED(Röd,Gul, Blå och Grön
-4 Resistorer 220 Ohm
-1 Resistor 2K Ohm
-1 Resistor 1,7K Ohm
-1 Piezosummer
-1 Bluetoothmodul HC-06
-*/
-//Bibliotek så jag kan använda digitala 3 och 5
-//istället för TX RX
 #include <SoftwareSerial.h>
+#include <math.h>
 
-//Bluetoothmodulen kopplad till TX -> 5 och RX ->3
 SoftwareSerial BT(3, 5);
 
-//Ge lamporna en färg.
 const byte red = 13;
 const byte yellow = 12;
 const byte blue = 11;
-const byte green = 10;
-const byte ljud = 9;
+const byte window = 7;
+const byte larm = 6;
+int tempPin = 0;
 
-/*Använder mig utav timers här för att undvika att använda
-delay() vilket gör att man kan t ex tända och släcka en lampa
-medan en annan blinkar*/
-const unsigned long blinkInterval = 500;
-const unsigned long asynkInterval = 200;
+long redTimer;
+long yellowTimer;
+long blueTimer;
 
-unsigned long yellowTimer;
-unsigned long blueTimer;
-unsigned long greenTimer;
+int larmState = 0;
+long larmTime = 0;
+long larmDelay = 200;
+boolean larmTriggered = false;
+boolean larmOn = false;
+
+
+int windowState = 0;
+long tempTime = 0;
+long tempDelay = 1000;
+int current = HIGH;
+int previous = LOW;
+
+int values[6] = {0, 0, 0, 0, 0, 0};
+long valuesTime = 0;
+long valuesDelay = 100;
+
 
 void setup() {
-  //Lampa som ska lysa
   pinMode(red, OUTPUT);
-  //Lampa som ska blinka
   pinMode(yellow, OUTPUT);
-  //Lampor som ska blinka asynkront
   pinMode(blue, OUTPUT);
-  pinMode(green, OUTPUT);
-  //Piezosummer för ljud
-  pinMode(ljud, OUTPUT);
 
-  //Sätt timers till nuvarande millisekundsvärde.
+  redTimer = millis();
   yellowTimer = millis();
-  blueTimer   = millis();
-  greenTimer  = millis();
+  blueTimer = millis();
 
-  //Initiera Bluetooth
   BT.begin(9600);
-  BT.println("Bluetooth ansluten!");
 }
 
-//Function för att blinka gula lampan.
-void yellowBlink () {
-  if (digitalRead (yellow) == LOW)
-      digitalWrite (yellow, HIGH);
-   else
-      digitalWrite (yellow, LOW);
-
-  // Komma ihåg när vi startade
-  yellowTimer = millis ();  
-  }
-
-//Function för att blinka blå lampan.
-void blueBlink () {
-  if (digitalRead (blue) == LOW)
-      digitalWrite (blue, HIGH);
-   else
-      digitalWrite (blue, LOW);
-
-  // Komma ihåg när vi startade
-  blueTimer = millis ();  
-  }
-
-//Function för att blinka gröna lampan.
-void greenBlink () {
-  if (digitalRead (green) == LOW)
-      digitalWrite (green, HIGH);
-   else
-      digitalWrite (green, LOW);
-
-  // Komma ihåg när vi startade
-  greenTimer = millis ();  
-  }
-  
-//Kommandot som skickas från Android sparas i command.
 char buffer;
 
-//Booleans för att släcka/tända och ljud på/av
-boolean blinkaGul = false;
-boolean blinkaBla = false;
-boolean blinkaGron = false;
-
 void loop() {
-
-  //Om nuvarande millis() är större eller lika med intervallet uppdateras lampans output.
-  if ( (millis() - yellowTimer) >= blinkInterval) {
-        if(blinkaGul){
-        yellowBlink();}
+  if(BT.available()) {
+    buffer = (BT.read());
+    switch(buffer) {
+      case '1' : {
+        if(digitalRead(red) == LOW){
+          digitalWrite(red, HIGH);
+          values[0] = 1;
         }
-  if ( (millis() - blueTimer) >= blinkInterval) {
-        if(blinkaBla){
-        blueBlink();}
+        else {
+          digitalWrite(red, LOW);
+          values[0] = 0;
         }
-  if ( (millis() - greenTimer) >= asynkInterval) {
-        if(blinkaGron){
-        greenBlink();}
+        break;
+      }
+      case '2' : {
+        if(digitalRead(yellow) == LOW) {
+          digitalWrite(yellow, HIGH);
+          values[1] = 1;
         }
-
-  //Alla olika kommandon som kan skickas till arduinon        
-  if (BT.available()){
-   buffer = (BT.read());
-
-      //Tänd Röd
-      if(buffer == '1') {
-        digitalWrite(red, HIGH);
+        else {
+          digitalWrite(yellow, LOW);
+          values[1] = 0;
+        }
+        break;
       }
-      //Släck Röd
-      if(buffer == '2') {
-        digitalWrite(red, LOW);
+      case '3' : {
+        if(digitalRead(blue) == LOW) {
+          digitalWrite(blue, HIGH);
+          values[2] = 1;
+        }
+        else {
+          digitalWrite(blue, LOW);
+          values[2] = 0;
+        }
+        break;
       }
-      //Blinka Gul
-      if(buffer == '3') {
-        blinkaGul = true;
+      case '4' : {
+        if(larmOn != true) {
+          larmOn = true;
+          values[5] = 1;
+        }
+        else {
+          larmOn = false;
+          values[5] = 0;              
+        }
+        break;
       }
-      //Släck Gul
-      if(buffer == '4') {
-        blinkaGul = false;
-        digitalWrite(yellow, LOW);
-      }
-      // Blinka Blå och Grön
-      if(buffer == '5') {
-        blinkaBla = true;
-        blinkaGron = true;        
-      }
-      //Släck Blå och Grön
-      if(buffer == '6') {
-        blinkaBla = false;
-        blinkaGron = false;
-        digitalWrite(blue, LOW);
-        digitalWrite(green, LOW);
-      }
-      //Starta ljud
-      if(buffer == '7') {
-        //Spelar ljud med frekvens på 31Hz
-        tone(ljud, 31);
-      }
-      //Stäng av ljud
-      if(buffer == '8') {
-        noTone(ljud);
-      }
-
+    }
   }
-
+  
+  checkWindow();
+  checkLarm();
+  checkTemp();
+  sendAndroidValues();
 }
+
+void checkTemp() {
+  
+  int sensor = analogRead(tempPin);  
+  float tempVal = (sensor * 5.0 / 1024.0 - 0.5) * 100;
+  round(tempVal);
+  int roundTemp = tempVal;
+  values[3] = roundTemp;
+  
+  if(millis() - tempTime > tempDelay){
+    tempTime = millis();
+  }
+  
+}
+
+void checkWindow() {
+  windowState = digitalRead(window);
+  if(windowState == HIGH) {
+    values[4] = 1;
+  } else {
+    values[4] = 0; 
+  }
+    
+}
+
+void checkLarm() {
+  larmState = digitalRead(larm);
+  if(larmOn == true) {
+  if(larmState == HIGH && previous == LOW && millis() - larmTime > larmDelay) {
+    if (current == HIGH) current = LOW;
+    else current = HIGH;
+    if(current == HIGH) larmTriggered = true;
+    else larmTriggered = false;
+
+    larmTime = millis();
+  }
+  }
+  
+  previous = larmState; 
+  
+}
+
+void readAndroidValues() {
+  
+}
+
+void sendAndroidValues() {
+  if(millis() - valuesTime > valuesDelay) {
+  BT.print('P');
+  for(int k = 0; k<7; k++) {
+    BT.print(values[k]);
+  }
+  BT.print('Q');
+  BT.println();
+  valuesTime = millis();
+  }
+  
+}
+
